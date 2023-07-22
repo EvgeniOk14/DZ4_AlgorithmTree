e8ab35e68d5ed2ec436ddaf4f88bfd9164f36705
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-07-22 21:11:57 +0300

    created a new project: ReadBlackBinaryTree

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+        tree.add(3);
+        tree.add(8);
+        tree.add(9);
+        tree.add(15);
+        tree.add(21);
+        tree.add(50);
+        tree.add(20);
+        tree.add(40);
+        tree.add(30);
+        tree.print();
+    }
+}
